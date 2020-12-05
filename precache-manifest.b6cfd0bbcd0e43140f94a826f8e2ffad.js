3e27b3f701d8cc3a63861b9d5036118a7f80ef1d
Madhur Gupta <guptamadhur96@gmail.com>
2020-12-05 18:06:26 +0530

    Updates

---
+++ precache-manifest.b6cfd0bbcd0e43140f94a826f8e2ffad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2bda8ec2a469c7cd9df51e14cd3c627",
+    "url": "/Diamond-Sweeper/index.html"
+  },
+  {
+    "revision": "8b6a619dcc3420b06acc",
+    "url": "/Diamond-Sweeper/static/css/main.cad1c058.chunk.css"
+  },
+  {
+    "revision": "a431358f159e4862ac04",
+    "url": "/Diamond-Sweeper/static/js/2.8dfe8bdb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Diamond-Sweeper/static/js/2.8dfe8bdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b6a619dcc3420b06acc",
+    "url": "/Diamond-Sweeper/static/js/main.accaaa82.chunk.js"
+  },
+  {
+    "revision": "8501625ae042ac517568",
+    "url": "/Diamond-Sweeper/static/js/runtime-main.6f5274c2.js"
+  }
+]);
